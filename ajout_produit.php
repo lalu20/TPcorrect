5237b57736d1a5539ce1bb8c437a1ce278d5caf5
unknown <f.zanoune@aol.com>
2022-11-17 15:00:40 +0100

    Première Initialisation

--- ajout_produit.php
+++ ajout_produit.php
@@ -1,1 +1,8 @@
-                                                                                                                                               
+ <?php
+ 
+ /* 4 status de fichier git: 
+ 1 untracked=> fichiers non suivis par git
+ 2 tracked=> fichiers déja suivi par git mais non envoyés
+ vers les repository
+ 3 staged => les fichiers , enregistrès mais non envoyés vers le repository
+ 4  committed=> */ 
